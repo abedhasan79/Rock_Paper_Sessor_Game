f6423e3ba57e37b6c44f8d6fda583861180313ea
Abed Hasan <abedhasan79@gmail.com>
2022-09-15 23:29:07 -0400

    now keeps track of win loss and tie

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,52 @@
 
-let browserPrompt = prompt("Enter r or p or s");
-
-let getRandom = Math.floor(Math.random() * 3);
-
-console.log(getRandom);
+let browserPrompt;
 let computerChoice;
-if(getRandom==0){
-    computerChoice="r";
-}else if(getRandom==1){
-    computerChoice="p";
-}else{
-    computerChoice="s";
-}
 
-alert("Computer Choice is "+computerChoice);
+let win=0;
+let loss=0;
+let tie=0;
 
-function compareChoices(browserPrompt, computerChoice){
-    if(browserPrompt=="r" || browserPrompt=="s" || browserPrompt=="p"){
-        if(computerChoice==browserPrompt){
+function compareChoices(browserPrompt, computerChoice) {
+    browserPrompt = prompt("Enter r or p or s");
+    let getRandom = Math.floor(Math.random() * 3);
+
+    console.log(getRandom);
+    
+    if (getRandom === 0) {
+        computerChoice = "r";
+    } else if (getRandom === 1) {
+        computerChoice = "p";
+    } else {
+        computerChoice = "s";
+    }
+
+    alert("Computer Choice is " + computerChoice);
+
+    if (browserPrompt === "r" || browserPrompt === "s" || browserPrompt === "p") {
+        if (computerChoice == browserPrompt) {
+            tie+=1;
             alert("It's a Tie");
-        }else if( browserPrompt=="p" && computerChoice=="r"){
+        } else if (browserPrompt === "p" && computerChoice === "r") {
+            win+=1;
             alert("You Win!");
-        }else if(browserPrompt=="s" && computerChoice=="p"){
+        } else if (browserPrompt === "s" && computerChoice === "p") {
+            win+=1;
             alert("You Win!");
-        }else if(browserPrompt=="r" && computerChoice=="s"){
+        } else if (browserPrompt === "r" && computerChoice === "s") {
+            win+=1;
             alert("You Win!");
-        }else{
+        } else {
+            loss+=1;
             alert("Computer Wins!");
-        }   
-    }else{
+        }
+    } else {
         alert("You Must Enter r or p or s");
     }
-    if(window.confirm("Do you want to play again?")){
-        location.reload();
+
+    alert("Your Win: "+win+ ", Your Loss: "+loss+ ", Ties: "+tie);
+
+    if (window.confirm("Do you want to play again?")) {
+        compareChoices(browserPrompt, computerChoice);
     }
     return;
 }
